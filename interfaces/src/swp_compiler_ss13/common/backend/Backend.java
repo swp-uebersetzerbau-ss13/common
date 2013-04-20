9fb7047e27e5433d31d2d15392b11264c1d21c6c
Moritz Maxeiner <moritz@ucworks.org>
2013-04-20 20:04:37 +0200

    Change backend interface to not get an OutputStream to write to, but to return a list of InputStreams mapped with names, that contain the target code; these can then be read from to get the target code, e.g. so the controller can write it to a file..

--- interfaces/src/swp_compiler_ss13/common/backend/Backend.java
+++ interfaces/src/swp_compiler_ss13/common/backend/Backend.java
@@ -1,9 +1,10 @@
 package swp_compiler_ss13.common.backend;
 
-import java.io.OutputStream;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 
 public interface Backend
 {
-	void generateTargetCode(List<Quadruple> tac, OutputStream output);
+	Map<String,InputStream> generateTargetCode(List<Quadruple> tac);
 }
