88ba8a93534a36a96066c7a9a65d089289c48e8f
dildik <ddurmaz88@gmail.com>
2013-04-21 21:37:42 +0200

    added interface proposal for lexer

---
+++ interfaces/src/swp_compiler_ss13/common/lexer/Lexer.java
@@ -0,0 +1,18 @@
+package swp_compiler_ss13.common.lexer;
+
+/**
+ * Proposed interface for symbol table
+ * 
+ * @author "Damla Durmaz", "Ferhat Beyaz", "Sebastian Barthel"
+ * @version 1
+ * @see <a target="_top" href="https://github.com/swp-uebersetzerbau-ss13/common/issues/3</a>
+ */
+public interface Lexer {
+	
+	/**
+	 * Get the next token of the source code. If there is no token, this method returns null.
+	 * @return A Token, representing the next valid lexeme of the source code
+	 */
+	public Token getNextToken();
+	
+}
