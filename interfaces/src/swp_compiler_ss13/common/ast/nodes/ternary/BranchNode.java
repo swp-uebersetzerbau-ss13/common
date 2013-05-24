d00996fde4a57f13bb008863e7656f4419c5d106
Tilman Krauß <Tilman.krauss@gmail.com>
2013-05-24 10:25:54 +0200

    fixed little naming convention violence

--- interfaces/src/swp_compiler_ss13/common/ast/nodes/ternary/BranchNode.java
+++ interfaces/src/swp_compiler_ss13/common/ast/nodes/ternary/BranchNode.java
@@ -27,36 +27,36 @@ public interface BranchNode extends StatementNode
 	public ExpressionNode getCondition();
 
 	/**
-	 * Set the block that is executed when the condition evaluates to true.
+	 * Set the statement that is executed when the condition evaluates to true.
 	 * 
-	 * @param block
-	 *            The block to execute when the condition evaluates to true.
+	 * @param statement
+	 *            The statement to execute when the condition evaluates to true.
 	 */
-	public void setBlockNodeOnTrue(StatementNode block);
+	public void setStatementNodeOnTrue(StatementNode statement);
 
 	/**
-	 * The block that is executed when the condition evaluates to true.
+	 * The statement that is executed when the condition evaluates to true.
 	 * 
-	 * @return The block to execute when the condition evaluates to true.
+	 * @return The statement to execute when the condition evaluates to true.
 	 */
-	public StatementNode getBlockNodeOnTrue();
+	public StatementNode getStatementNodeOnTrue();
 
 	/**
-	 * Set the block that is executed when the condition evaluates to flase.
-	 * This is the else branch. If there is no else an empty block should be
+	 * Set the statement that is executed when the condition evaluates to false.
+	 * This is the else statement. If there is no else an empty block should be
 	 * set.
 	 * 
-	 * @param block
-	 *            The block to execute when the condition evaluates to false or
-	 *            an empty block if no else branch is needed.
+	 * @param statement
+	 *            The statement to execute when the condition evaluates to false or
+	 *            an empty statement if no else branch is needed.
 	 */
-	public void setBlockNodeOnFalse(StatementNode block);
+	public void setStatementNodeOnFalse(StatementNode statement);
 
 	/**
-	 * The block that is executed when the condition is false. This is the else
+	 * The statement that is executed when the condition is false. This is the else
 	 * branch. If no else branch exists this will return an empty block.
 	 * 
-	 * @return The block to execute when the condition evaluates to false.
+	 * @return The statement to execute when the condition evaluates to false.
 	 */
-	public StatementNode getBlockNodeOnFalse();
+	public StatementNode getStatementNodeOnFalse();
 }
