9c23a7dab8b8295965a11119b17a9ec176184b7a
SWP-Comp-Ch3ck3r <frank.zechert@fu-berlin.de>
2013-05-30 23:44:21 +0200

    added interface for semanticAnalyzer, see issue #26

---
+++ interfaces/src/swp_compiler_ss13/common/semanticAnalysis/SemanticAnalyser.java
@@ -0,0 +1,9 @@
+package swp_compiler_ss13.common.semanticAnalysis;
+
+import swp_compiler_ss13.common.ast.AST;
+import swp_compiler_ss13.common.parser.ReportLog;
+
+public interface SemanticAnalyser {
+	 public void setReportLog(ReportLog log);
+	 public AST analyse(AST ast);
+}
