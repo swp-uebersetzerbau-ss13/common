fcce2d2dbf5b9f3c829448ee7fc6f2111d4841e1
gero <geroleinemann@gmx.de>
2013-07-04 18:25:36 +0200

    Updated StructType as discussed today

--- interfaces/src/swp_compiler_ss13/common/types/derived/StructType.java
+++ interfaces/src/swp_compiler_ss13/common/types/derived/StructType.java
@@ -1,6 +1,5 @@
 package swp_compiler_ss13.common.types.derived;
 
-import swp_compiler_ss13.common.types.Type;
 
 /**
  * Struct type definition.
@@ -18,16 +17,11 @@ public class StructType extends DerivedType
 	protected Member[] members;
 
 	/**
-	 * The name of the struct type.
-	 */
-	protected String name;
-
-	/**
 	 * Create a new struct type.
 	 * @param typeName The name of this type.
 	 * @param members The member types.
 	 */
-	public StructType(String typeName, Member... members)
+	public StructType(Member... members)
 	{
 		super(Kind.STRUCT);
 		if (members.length <= 0)
@@ -35,7 +29,6 @@ public class StructType extends DerivedType
 			throw new IllegalArgumentException("A struct needs to have at least one member.");
 		}
 
-		this.name = typeName;
 		this.members = members;
 
 		this.width = 0L;
@@ -58,15 +51,6 @@ public class StructType extends DerivedType
 	 * {@inheritDoc}
 	 */
 	@Override
-	public String getTypeName()
-	{
-		return this.name;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
 	public String toString()
 	{
 		StringBuilder typelist = new StringBuilder();
@@ -77,6 +61,6 @@ public class StructType extends DerivedType
 		}
 
 		String types = typelist.substring(0, typelist.length() - 2);
-		return String.format("%s {%s}", this.name, types);
+		return String.format("%s {%s}", this.getTypeName(), types);
 	}
 }
